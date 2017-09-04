77e2332fd945ab2f9ade808fa1e8cb0a24c12d9e
Akashi_SN <btorntireinvynriy@gmail.com>
2017-09-04 23:17:25 +0900

    Update

---
+++ q12/q12.py
@@ -0,0 +1,7 @@
+import hpack
+
+decoder = hpack.Decoder()
+data = decoder.decode("88c3c24085f2b4b40e6f94dd82f5ed5a7fff63ff7feeb119b46a200fff3ffd0f0d023137c1c0bf4089f2b4e94d625abb513f0131".decode('hex'))
+
+encoder = hpack.Encoder()
+print encoder.encode(data).encode('hex')
